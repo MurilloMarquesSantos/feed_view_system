6cc6b11df41b5681dc59b4086178ca42261bd4fb
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:42:04 -0300

    feat: add method to retrieve products

--- user-service/src/main/java/dev/marques/controller/UserController.java
+++ user-service/src/main/java/dev/marques/controller/UserController.java
@@ -1,11 +1,23 @@
 package dev.marques.controller;
 
+import dev.marques.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/home")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(userService.listProducts());
+    }
 
 
 }
