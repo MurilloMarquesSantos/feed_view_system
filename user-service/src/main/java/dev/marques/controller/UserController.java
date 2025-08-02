b259f86a60f480954b1e37343b0f859bf608507c
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:40:06 -0300

    feat: add User Controller class

---
+++ user-service/src/main/java/dev/marques/controller/UserController.java
@@ -0,0 +1,11 @@
+package dev.marques.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+
+}
