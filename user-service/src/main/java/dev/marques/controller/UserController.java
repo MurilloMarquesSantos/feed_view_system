2d38c44d13b8dda090d4110fe395e91b6b734915
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:44:12 -0300

    feat: add product class import

--- user-service/src/main/java/dev/marques/controller/UserController.java
+++ user-service/src/main/java/dev/marques/controller/UserController.java
@@ -1,11 +1,14 @@
 package dev.marques.controller;
 
+import dev.marques.dto.Product;
 import dev.marques.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
