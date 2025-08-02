b8529618f073844ad2b51476b5db846de7bfdd34
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:33:26 -0300

    feat: add product service class with getAll() method

---
+++ database-service/src/main/java/dev/marques/service/ProductService.java
@@ -0,0 +1,19 @@
+package dev.marques.service;
+
+import dev.marques.domain.Product;
+import dev.marques.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
