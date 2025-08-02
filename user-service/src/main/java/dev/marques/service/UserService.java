29247666c05c25d3482943cdb6c58e6a6b4bcaa9
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 15:32:46 -0300

    remove: delete log4j2 usages

--- user-service/src/main/java/dev/marques/service/UserService.java
+++ user-service/src/main/java/dev/marques/service/UserService.java
@@ -2,7 +2,6 @@ package dev.marques.service;
 
 import dev.marques.dto.Product;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +12,15 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Log4j2
 public class UserService {
 
     private final RestTemplate restTemplate;
-
     public List<Product> listProducts() {
         ResponseEntity<List<Product>> response = restTemplate.exchange("http://localhost:8081/home",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<>() {
                 });
-        log.info(response);
         return response.getBody();
 
     }
