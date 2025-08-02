b7c4435820d42f0e3c1d3de851f4afa87b69cad6
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:33:52 -0300

    feat: add product controller class with getProducts() endpoint

---
+++ database-service/src/main/java/dev/marques/controller/ProductController.java
@@ -0,0 +1,24 @@
+package dev.marques.controller;
+
+import dev.marques.domain.Product;
+import dev.marques.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/home")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getProducts());
+    }
+}
