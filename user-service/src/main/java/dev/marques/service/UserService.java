1c5cad98937eb9b399a37df91db26fd0ced6a6c1
Murillo Marques <murillomarques2001@gmail.com>
2025-08-02 14:57:40 -0300

    feat: add method to retrieve data

--- user-service/src/main/java/dev/marques/service/UserService.java
+++ user-service/src/main/java/dev/marques/service/UserService.java
@@ -1,9 +1,28 @@
 package dev.marques.service;
 
+import dev.marques.dto.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final RestTemplate restTemplate;
 
+    public List<Product> listProducts() {
+        ResponseEntity<List<Product>> response = restTemplate.exchange("https://localhost:8081/home",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+        return response.getBody();
+
+    }
 }
