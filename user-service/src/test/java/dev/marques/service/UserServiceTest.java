73ebe6bf8f2d04362b3509e2b6683a013493bcfb
Murillo Marques <murillomarques2001@gmail.com>
2025-08-04 18:41:11 -0300

    test: add test for listProducts method

---
+++ user-service/src/test/java/dev/marques/service/UserServiceTest.java
@@ -0,0 +1,31 @@
+package dev.marques.service;
+
+import dev.marques.dto.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserService userService;
+
+
+    @Test
+    void listProducts_ReturnsListOfProduct_WhenSuccessful(){
+
+        BDDMockito.when(userService.listProducts()).thenReturn(List.of(new Product()));
+
+        List<Product> products = userService.listProducts();
+
+        assertThat(products).hasSize(1);
+
+    }
+}
