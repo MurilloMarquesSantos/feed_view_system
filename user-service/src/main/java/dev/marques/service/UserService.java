9c7541c2943f6492d425110cfda81143046eec23
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:42:13 -0300

    feat: add method to use redis cache

--- user-service/src/main/java/dev/marques/service/UserService.java
+++ user-service/src/main/java/dev/marques/service/UserService.java
@@ -1,12 +1,9 @@
 package dev.marques.service;
 
 import dev.marques.dto.Product;
+import dev.marques.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,14 +11,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final RestTemplate restTemplate;
-    public List<Product> listProducts() {
-        ResponseEntity<List<Product>> response = restTemplate.exchange("http://localhost:8081/home",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                });
-        return response.getBody();
+//    private final RestTemplate restTemplate;
+//
+//    public List<Product> listProducts() {
+//        ResponseEntity<List<Product>> response = restTemplate.exchange("http://localhost:8081/home",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<>() {
+//                });
+//        return response.getBody();
+//
+//    }
 
+    private final RedisService redisService;
+
+    private final JsonUtil jsonUtil;
+
+    public List<Product> listProducts() {
+        String productList = redisService.getProductList();
+        return jsonUtil.toProductList(productList);
     }
 }
