b21bb78a0be323a1fc41dcbc2b659964c45f215c
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:24:35 -0300

    feat: add save method

---
+++ user-service/src/main/java/dev/marques/service/RedisService.java
@@ -0,0 +1,21 @@
+package dev.marques.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final String CACHE_KEY = "product-list";
+
+    public void saveAll(String productsList) {
+        stringRedisTemplate.opsForValue().set(CACHE_KEY, productsList, 10, TimeUnit.MINUTES);
+    }
+
+}
