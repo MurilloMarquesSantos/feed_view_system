679f20c35143bdc9609747a96b863a1733ec7b83
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:28:07 -0300

    feat: add saveAll method to consumer

--- user-service/src/main/java/dev/marques/listener/KafkaConsumer.java
+++ user-service/src/main/java/dev/marques/listener/KafkaConsumer.java
@@ -1,24 +1,21 @@
 package dev.marques.listener;
 
-import dev.marques.dto.Product;
-import dev.marques.util.JsonUtil;
+import dev.marques.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
-    private final JsonUtil jsonUtil;
+    private final RedisService redisService;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topic.feed}"
     )
     public void consumerFeedTopic(String payload) {
-        List<Product> productList = jsonUtil.toProductList(payload);
+        redisService.saveAll(payload);
     }
 }
