f69d54d064977ee03b09d66353fe1b04ce1872c3
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:22:01 -0300

    fix: update redis config

--- user-service/src/main/java/dev/marques/config/RedisConfig.java
+++ user-service/src/main/java/dev/marques/config/RedisConfig.java
@@ -1,22 +1,15 @@
 package dev.marques.config;
 
-import dev.marques.dto.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-        @Bean
-        public RedisTemplate<String, Product> redisTemplate(RedisConnectionFactory connectionFactory) {
-            RedisTemplate<String, Product> template = new RedisTemplate<>();
-            template.setConnectionFactory(connectionFactory);
-            template.setKeySerializer(new StringRedisSerializer());
-            template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Product.class));
-            return template;
-        }
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
 }
