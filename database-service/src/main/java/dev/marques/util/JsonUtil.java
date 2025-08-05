2b1c5db1aba8c35619f949d8c777c363ad19989a
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:27:47 -0300

    feat: add jsonUtil class

---
+++ database-service/src/main/java/dev/marques/util/JsonUtil.java
@@ -0,0 +1,40 @@
+package dev.marques.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.marques.domain.Product;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("Error while converting object to json", e);
+            return null;
+        }
+    }
+
+    public List<Product> toProductList(String payload) {
+        try {
+            return objectMapper.readValue(payload, new TypeReference<List<Product>>() {
+            });
+        } catch (JsonProcessingException e) {
+            log.error("Error while converting json to product list", e);
+            return List.of();
+        }
+    }
+
+
+}
