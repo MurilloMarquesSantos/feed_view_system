1e8d09072cf6025cc5c487bdc2e495b22cf39e33
Murillo Marques <murillomarques2001@gmail.com>
2025-08-05 11:48:41 -0300

    feat: add consumer method

---
+++ database-service/src/main/java/dev/marques/producer/ProducerService.java
@@ -0,0 +1,39 @@
+package dev.marques.producer;
+
+import dev.marques.domain.Product;
+import dev.marques.repository.ProductRepository;
+import dev.marques.util.JsonUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Log4j2
+public class ProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private final JsonUtil jsonUtil;
+
+    private final ProductRepository productRepository;
+
+    @Value("${spring.kafka.topic.feed}")
+    private String feedTopic;
+
+    public void sendMessage() {
+        List<Product> productList = productRepository.findAll();
+
+        String payload = jsonUtil.toJson(productList);
+        try {
+            kafkaTemplate.send(feedTopic, payload);
+        } catch (Exception e) {
+            log.error("FAIL: ", e.getMessage());
+            log.error(e);
+        }
+    }
+}
